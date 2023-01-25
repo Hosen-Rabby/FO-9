c37f1f10b5d5ae3a43366412d0fa3a7f1373b944
Hosen Rabby <mdhosen21018@gmail.com>
2023-01-25 12:36:41 +0600

    updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,3 +31,23 @@ $('.gallery-slider').slick({
 
   ]
 });
+
+// preloader
+var loader = document.getElementById("preloader");
+window.addEventListener("load", function () {
+  loader.style.display = "none";
+});
+
+// scrop to top
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 300) {
+      $(".to_top").fadeIn(250);
+    } else {
+      $(".to_top").fadeOut(250);
+    }
+  });
+  $(".to_top").click(function () {
+    $("html,body").animate({ scrollTop: 0 }, 400);
+  });
+});
